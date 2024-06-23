c6c2725925b6587cfa279fe728ecd3d23607bbd2
juliaret <julia.retuertanogales@alumnostrazos.net>
2024-06-23 18:25:38 +0200

    practica-casa-23-junio

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,28 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+// PASOS
+
+//Sincronizar el length del nuevo password con el input range
+//Que al cambiar el range, se refleje en el length
+//Que al darle al botón de generar contraseña, genere una contraseña
+//Que al darle al botón de generar contraseña, se genere una contrseña de la longitud que le hemos dicho
+
+const availableCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+-={}[]:;<>,.?/';
+
+const rangeLabelElement = document.getElementById('range-label');
+const rangeInputElement = document.getElementById('range');
+
+const updateLabel = () => {
+  rangeLabelElement.textContent = `LENGTH: ${rangeInputElement.value}`;
+};
+
+rangeInputElement.addEventListener('input', updateLabel);
+
+const generatePasswordButtonElement = document.getElementById('generate-password-button');
+
+const generatePassword = () => {
+    const randomCharacter = Math.floor(Math.random())
+};
+
+generatePasswordButtonElement.addEventListener('click', generatePassword);
